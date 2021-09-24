b1a242c0b15f046b611e6392c02f0a3e98afd757
Radar3759 <bunnybridgemitten@gmail.com>
2021-09-24 06:32:57 -0700

    Create avg_height.py

---
+++ avg_height.py
@@ -0,0 +1,20 @@
+# 🚨 Don't change the code below 👇
+student_heights = input("Input a list of student heights ").split()
+for n in range(0, len(student_heights)):
+  student_heights[n] = int(student_heights[n])
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+#use a for loop
+total_height = 0
+total_students = 0
+
+for height in student_heights:
+  total_height += height
+  total_students += 1
+  avg_height =round(total_height / total_students)
+
+  
+# print(total_height)
+# print(total_students)
+print(f"The average height is: {avg_height}.")
