70f1f1848dca19321d151451379a1ebba4b056be
Radar3759 <bunnybridgemitten@gmail.com>
2021-09-24 09:10:28 -0700

    Create add_evens.py

---
+++ add_evens.py
@@ -0,0 +1,7 @@
+#Write your code below this row 👇
+#prints only even numbers in the range
+total = 0
+#range is NOT inclusive of the end number
+for number in range (1, 101, 2):
+  total += number
+print(total)
